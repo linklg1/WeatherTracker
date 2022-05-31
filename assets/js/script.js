70ce0842a4dfe34a5de69a8f8680f92b8000c29e
Zelda <lumakawar@hotmail.com>
2022-05-30 22:32:11 -0400

    initalHtmlandCss

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
 $(document).ready(function () {
 
- let Today = moment().format("LL")   
+ let Today = moment().format("LL"); 
+ let weatherdata = document.querySelector("#weather-data")  
+let submit = document.querySelector("#submit")
 
-});
+runfiveday = function(){
+    event.preventDefault();
+    weatherdata.setAttribute("style", "display: block;");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ submit.addEventListener("click", runfiveday)
+
+ });
