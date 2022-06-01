ca6f7b1d237937605c6c419cfd52a8dd614eb194
Zelda <lumakawar@hotmail.com>
2022-06-01 09:16:04 -0400

    reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function () { 
  
     // Global variables //
 
@@ -11,86 +11,30 @@ let day3 = moment().add(3, "days").format("LL");
 let day4 = moment().add(4, "days").format("LL");
 let day5 = moment().add(5, "days").format("LL");
 
-
 //other//
 let city;
 let cities; 
-let weatherdata = document.querySelector("#weather-data")  
-let submit = document.querySelector("#submit")
+let weatherdata = document.getElementById("weather-data")  
+let submit = document.getElementById("submit")
+
 
 
 
  //Functions//
 
- //Load history from local storage re: city searched//
- function loadRecentsone() {
-    let lastSearch = localStorage.getItem("Recents");
-    if (lastSearch) {
-      city = lastSearch;
-      search();
-    } else {
-      city = "Toronto";
-      search();
-    }
-  };
+ 
+ // display 5 days cards //
 
-  loadRecentsone()
-
-//Load history from local storage re: cities searched//
-
-  function loadRecentsmany() {
-    let recentCities = JSON.parse(localStorage.getItem("cities"));
-
-    if (recentCities) {
-      cities = recentCities;
-    } else {
-      cities = [];
-    }
-  };
-
-  loadRecentsmany()
-
-// display 5 days cards //
-runfiveday = function(){
-    event.preventDefault();
-    weatherdata.setAttribute("style", "display: block;");
-    getCity();
-    search();
-    document.querySelector("#city-input").val("");
-    listCities();
-
-};
-
-
-// save searched cities to local storage
-  function saveToLocalStorage() {
-    localStorage.setItem("Recents", city);
-    cities.push(city);
-    localStorage.setItem("cities", JSON.stringify(cities));
-  };
-
-
-  // retrieve user input
-  function getCity() {
-    city = document.querySelector("#city-input").val();
-    if (city && cities.includes(city) === false) {
-      saveToLocalStorage();
-      return city;
+runfiveday = function() {
+   weatherdata.setAttribute("style", "display: block;");
     
-  }};
-
-//Run API
-function search () {}
-
-
-
 
 
 
 
 
 //Event Listeners //
- submit.addEventListener("click", runfiveday)
+ submit.addEventListener("click", runfiveday);
 
 
 });
